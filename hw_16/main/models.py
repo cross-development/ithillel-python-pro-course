d2dbd91e04fd4f5185b3f5ef825603f957b48fb1
Vitalii Derda <derdavitaliy@gmail.com>
2025-03-13 18:33:26 +0200

    feat: added hw 16

---
+++ hw_16/main/models.py
@@ -0,0 +1,8 @@
+"""
+Database models for the main application.
+
+This module defines the database models used in the application. Each model
+corresponds to a table in the database.
+"""
+
+# from django.db import models
